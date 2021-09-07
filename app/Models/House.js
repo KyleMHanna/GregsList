9fc26896a0200275d01ff3592d6239ee8acc6c33
Kyle Hanna <Hanna.Kyle@live.com>
2021-09-07 14:42:54 -0600

    House module updated

--- app/Models/House.js
+++ app/Models/House.js
@@ -1,30 +1,30 @@
-import { generateId } from "../Utils/generateId.js"
+
 export class House {
   constructor(houseData) {
-    this.id = houseData.id || generateId()
+    this.id = houseData.id 
     this.price =houseData.price
-    this.address = houseData.address
-    this.rooms = houseData.rooms
+    this.year = houseData.year
+    this.levels = houseData.levels
+    this.bedrooms = houseData.bedrooms
     this.bathrooms = houseData.bathrooms
-    this.sqft = houseData.sqft
-    this.img = houseData.img
-    this.details =houseData.details
+    this.imgUrl = houseData.imgUrl
+    this.description =houseData.description
 }
 
 get HouseTemplate(){
     return /*html*/`
     <div class="col-lg-3 mb-4 listing">
       <div class="card">
-        <img src="${this.img}" alt="listing image" class="rounded">
+        <img src="${this.imgUrl}" alt="listing image" class="rounded">
         <div class="card-body">
-        <h3>  <span> ${this.address} </span></h3>
+        <h3>  <span> ${this.price} </span></h3>
           <h4 class=" justify-content-between">
-            <p><span>  ${this.rooms} Rooms</span></p>
+            <p><span>  ${this.bedrooms} Rooms</span></p>
             <p><span> ${this.bathrooms} Bathrooms</span></p>
-            <p><span> ${this.sqft} Sqft </span></p>
+        
             <p><span> Asking price $ ${this.price}</span></p>
             </h4>
-            <h5>Description: ${this.details}</h5>
+            <h5>Description: ${this.description}</h5>
         </div>
       </div>
     </div>
